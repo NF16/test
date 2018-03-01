a4186b7fecfd7977f58466a221efe2040fda01bd
NF16 <ttfeng0758@qq.com>
2018-03-01 15:39:06 +0800

    123

--- ConsoleApplication_Nlog/Program.cs
+++ ConsoleApplication_Nlog/Program.cs
@@ -11,14 +11,6 @@ namespace ConsoleApplication_Nlog
         static void Main(string[] args)
         {
             HistoryLog.SentMail("1");
-            HistoryLog.SentMail("1");
-            HistoryLog.SentMail("1");
-            HistoryLog.SentMail("1");
-            HistoryLog.SentMail("1");
-            HistoryLog.SentMail("1");
-            HistoryLog.SentMail("1");
-            HistoryLog.SentMail("1");
-            HistoryLog.SentMail("1");
             //HistoryLog.SendMail2("2");
             Console.ReadKey();
         }
