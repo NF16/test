d9badbbff1519800fb3201580c24212050afde47
NF16 <ttfeng0758@qq.com>
2019-06-25 23:07:44 +0800

    Update Form1.Designer.cs
    123

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -58,7 +58,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(164, 49);
+            this.button1.Location = new System.Drawing.Point(233, 46);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
